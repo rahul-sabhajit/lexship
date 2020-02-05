744eab2b61dbbb0f1608646fbb082b68d1fc2d7a
Rahul Patel <rahulpatel.mst@gmail.com>
2020-02-06 01:18:01 +0530

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,6 @@ use Illuminate\Http\Request;
     return $request->user()->orderBy('users.id', 'DESC')->paginate(3);
 });*/
 Route::middleware('auth:api')->get('/user', 'Api\TrackingController@userlist');
-Route::middleware('auth:api')->post('/calculateLcm', 'Api\TrackingController@computelcm');
-Route::middleware('auth:api')->get('/method', 'Api\TrackingController@algorithm');
 Route::middleware('auth:api')->get('/package', 'Api\TrackingController@package_details');
 Route::middleware('auth:api')->post('/package_details', 'Api\TrackingController@package_single');
 Route::middleware('auth:api')->post('/package_status_update', 'Api\TrackingController@updateStatus');
